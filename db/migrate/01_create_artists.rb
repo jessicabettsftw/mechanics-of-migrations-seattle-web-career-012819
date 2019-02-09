25e2c144ca5d0cb17f661a6e1dac449a3050489a
github username <github email address>
2019-02-08 21:31:41 -0800

    create DB connection(conf/env), create create_artist file(db/migrate)

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,18 @@
+class CreateArtists < ActiveRecord::Migration
+  def up
+    #define the code to execute when the migration is run
+  end
+
+  def down
+    #efine the code to execute when the migration is rolled back.
+  end
+
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+end
